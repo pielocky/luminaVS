6f05e23d80303429ec7624727ed25a577d442001
pielocky <vectorlocie@gmail.com>
2026-02-23 18:54:19 +0900

    Complete rewrite of buildozer.spec with all required fields

--- pythonforandroid/recipes/numpy/__init__.py
+++ pythonforandroid/recipes/numpy/__init__.py
@@ -2,7 +2,7 @@ from pythonforandroid.recipe import PythonRecipe
 
 class NumpyRecipe(PythonRecipe):
     version = '1.23.5'
-    url = 'https://pypi.org/packages/source/n/numpy/numpy-{version}.tar.gz'
+    url = 'https://mirror.yandex.ru/pypi/packages/source/n/numpy/numpy-1.23.5.tar.gz'
     depends = ['setuptools']
 
 recipe = NumpyRecipe()
--- pythonforandroid/recipes/numpy/init.py
+++
@@ -1,8 +0,0 @@
-from pythonforandroid.recipe import PythonRecipe
-
-class NumpyRecipe(PythonRecipe):
-    version = '1.23.5'
-    url = 'https://pypi.org/packages/source/n/numpy/numpy-{version}.tar.gz'
-    depends = ['setuptools']
-
-recipe = NumpyRecipe()
